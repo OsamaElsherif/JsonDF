a8b82f70a7d233b4614ca292fb6d515155dc8d7a
Osama Essam Mohammed <elsherifosama225@gmail.com>
2023-08-16 00:06:56 +0300

    added the insert method inside the Json type.

--- src/JsonDF/utils/Json/Json.py
+++ src/JsonDF/utils/Json/Json.py
@@ -10,6 +10,10 @@ class Json:
             self.__setattr__(key, value)
         return self
     
+    def insert(self, name, value):
+        self.__setattr__(name, value)
+        return self
+    
     def value(self, value):
         if type(value) == dict or type(value) == list:
             if type(value) == list:
@@ -24,6 +28,14 @@ class Json:
         for val in value:
             if type(val) == dict:
                 return Json(0, val).objectiy()
+    
+    def show(self):
+        attrs = {}
+        for attr in vars(self):
+            if attr.startswith('__') or callable(attr) or attr == 'json':
+                continue
+            attrs[attr] = getattr(self, attr)
+            return attrs
 
     def __repr__(self):
-        return self.json.__str__()
+        return self.show().__str__()

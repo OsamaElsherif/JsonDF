db0a4ecc37e727e4c1ea448ae24a7360e01a6ff8
Hussein Mukhtar <HushmKun@outlook.com>
2024-03-31 04:10:46 +0200

    1/2 a rework

---
+++ tests/test_deserializer.py
@@ -0,0 +1,18 @@
+import unittest
+
+
+class TestBoilerBuilder(unittest.TestCase):
+    def test_init(self):
+        pass
+
+    def test_build(self):
+        pass
+
+    def test_git(self): 
+        pass
+
+    def test_exception_handling(self):
+        pass
+
+if __name__ == "__main__":
+    pass
